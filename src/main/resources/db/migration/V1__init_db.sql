b24218ce8f9e1cd5aa4381720e88d7b5ce19bc5c
gominnam <kosok03@naver.com>
2024-01-26 13:39:25 +0900

    add flyway sql v1

---
+++ src/main/resources/db/migration/V1__init_db.sql
@@ -0,0 +1,20 @@
+
+drop table if exists user;
+drop table if exists hibernate_sequence;
+
+create table user (
+    id serial primary key,
+    name varchar(255) not null,
+    email varchar(255) not null,
+    password varchar(255) not null,
+    created_at timestamp not null default now(),
+    updated_at timestamp not null default now(),
+    role ENUM('ROLE_USER', 'ROLE_ADMIN'),
+    image_url varchar(255),
+    refresh_token varchar(255),
+    social_id varchar(255),
+    social_type ENUM('GOOGLE', 'NAVER', 'KAKAO')
+) engine=InnoDB;
+
+
+
