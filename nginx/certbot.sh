4ca2ca311c75204e394da988d3c62821821dbb04
gominnam <kosok03@naver.com>
2024-02-05 15:19:29 +0900

    edit integrating nginx and certbot in nginx/Dockerfile

---
+++ nginx/certbot.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Dockerfile에서 정의한 환경변수 사용
+EMAIL="${EMAIL}"
+
+# 인증서가 이미 존재하는지 확인
+if ! [ -e "/etc/letsencrypt/live/boosterko.kr/fullchain.pem" ]; then
+  # 인증서가 없으면 발급 받음
+  certbot certonly --webroot --webroot-path=/var/www/certbot --email "$EMAIL" --agree-tos --no-eff-email -d boosterko.kr -d www.boosterko.kr --deploy-hook "nginx -s reload"
+else
+  # 인증서가 있으면 갱신 시도
+  certbot renew --deploy-hook "nginx -s reload"
+fi
+
+# 갱신 실패시 30일 후 다시 시도
+trap exit TERM; while :; do sleep 720h & wait $${!}; done;
