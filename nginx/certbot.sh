695af93a986ef88e8aa0ed0723b9a6845abf1086
gominnam <kosok03@naver.com>
2024-02-06 08:07:08 +0900

    test certbot.sh light source

--- nginx/certbot.sh
+++ nginx/certbot.sh
@@ -4,13 +4,6 @@
 EMAIL="${EMAIL}"
 
 # 인증서가 이미 존재하는지 확인
-if ! [ -e "/etc/letsencrypt/live/boosterko.kr/fullchain.pem" ]; then
-  # 인증서가 없으면 발급 받음
-  certbot certonly --webroot -w /var/www/certbot --email "$EMAIL" --agree-tos --no-eff-email -d boosterko.kr -d www.boosterko.kr --deploy-hook "nginx -s reload"
-else
-  # 인증서가 있으면 갱신 시도
-  certbot renew --deploy-hook "nginx -s reload"
-fi
+certbot certonly --webroot -w /var/www/certbot --email "$EMAIL" --agree-tos --no-eff-email -d boosterko.kr -d www.boosterko.kr
 
-# 갱신 실패시 60일 후 다시 시도
-trap exit TERM; while :; do sleep 30d & wait ${!}; done;
+
