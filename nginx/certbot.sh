127279b363449b28ef06b9f73271b3dc47324773
gominnam <kosok03@naver.com>
2024-02-07 15:10:10 +0900

    edit deploy nginx-certbot docker settings

--- nginx/certbot.sh
+++ nginx/certbot.sh
@@ -4,6 +4,13 @@
 EMAIL="${EMAIL}"
 
 # 인증서가 이미 존재하는지 확인
-certbot certonly --webroot -w /var/www/certbot --email "$EMAIL" --agree-tos --no-eff-email -d boosterko.kr -d www.boosterko.kr
+if ! [ -e "/etc/letsencrypt/live/boosterko.kr/fullchain.pem" ]; then
+  # 인증서가 없으면 발급 받음
+  certbot certonly --webroot -w /var/www/certbot -d boosterko.kr -d www.boosterko.kr --email "$EMAIL" --agree-tos --no-eff-email --deploy-hook "nginx -s reload"
+else
+  # 인증서가 있으면 갱신 시도
+  certbot renew --deploy-hook "nginx -s reload"
+fi
 
-
+# 갱신 실패시 60일 후 다시 시도
+trap exit TERM; while :; do sleep 30d & wait ${!}; done;
